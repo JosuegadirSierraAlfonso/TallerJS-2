e94c804577c581278e2e88267742c4fe22317571
JosuegadirSierraAlfonso <josuegsierraa18@gmail.com>
2023-03-13 06:41:55 -0500

    feat: :rocket: excersice 7
    Programa que pida el ingreso del nombre y precio de un artículo y la cantidad que lleva el cliente. Mostrar lo que debe pagar el comprador en su factura.

--- main.js
+++ main.js
@@ -1,30 +1,20 @@
-//                  excersice 6
-/* Construir el algoritmo en Javascript para un programa
-para cualquier cantidad de estudiantes que lea el nombre,
-el sexo y la nota definitiva y halle al estudiante con la mayor
-nota y al estudiante con la menor nota y cuantos eran
-hombres y cuantos mujeres.*/
+//                  excersice 7
+/* Programa que pida el ingreso del nombre y precio de un artículo y la
+cantidad que lleva el cliente. Mostrar lo que debe pagar el comprador
+en su factura.*/
+
+let nProduct = ""
+let price = 0
+let cantidad = 0
+let totalFinal = 0
+do {    
+    nProduct = prompt("Ingrese nombre del producto: ") 
+    price = parseInt(prompt("Ingrese el precio: "))
+    cantidad = parseInt(prompt("Ingrese la cantidad de productos: "))
+
+    let total = price * cantidad
+    totalFinal += total
 
 
-
-
-let names = []
-let notes = []
-let gender = ""
-let m = []
-let f = []
-do {
-  names.push(prompt("Enter a name: "))
-  gender = (prompt("Enter your gender (M or F): "))
-  if (gender == "m" || gender == "M" ){
-     m.push(1)
-  }else if(gender == "f" || gender == "F"){
-     f.push(1)
-  }
-  notes.push(parseFloat(prompt("Enter a note: ")))
-} while (confirm("wanna continue? "));
-let bestNote = notes.indexOf(Math.max(...notes))
-let worseNote = notes.indexOf(Math.min(...notes))
-alert(`The student with the highest grade is ${names[bestNote]}\n
-The student with the lowest grade is ${names[worseNote]}\n
-There are ${m.length} men and ${f.length} women`)
+} while (confirm("Wanna continue?"))
+alert("El total a pagar es: "+totalFinal)
