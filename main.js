0ddf6cfe73a7f5b5bd26253c03990d19474f4a50
JosuegadirSierraAlfonso <josuegsierraa18@gmail.com>
2023-03-13 06:48:08 -0500

    feat: :rocket: excersice 7
    Programa que Ingrese por teclado: a. el valor del lado de un cuadrado para mostrar por pantalla el perímetro del mismo b. la base y la altura de un rectángulo para mostrar el área del mismo

--- main.js
+++ main.js
@@ -1,20 +1,12 @@
 //                  excersice 7
-/* Programa que pida el ingreso del nombre y precio de un artículo y la
-cantidad que lleva el cliente. Mostrar lo que debe pagar el comprador
-en su factura.*/
+/* Programa que Ingrese por teclado:
+a. el valor del lado de un cuadrado para mostrar por pantalla el
+perímetro del mismo
+b. la base y la altura de un rectángulo para mostrar el área del
+mismo*/
 
-let nProduct = ""
-let price = 0
-let cantidad = 0
-let totalFinal = 0
-do {    
-    nProduct = prompt("Ingrese nombre del producto: ") 
-    price = parseInt(prompt("Ingrese el precio: "))
-    cantidad = parseInt(prompt("Ingrese la cantidad de productos: "))
+let square = parseInt(prompt("Number of one side of the square: "));
+let rectangleHight = parseInt(prompt("Hight of the rectangle"));
+let rectangleWidht = parseInt(prompt("Widht of the rectangle"));
 
-    let total = price * cantidad
-    totalFinal += total
-
-
-} while (confirm("Wanna continue?"))
-alert("El total a pagar es: "+totalFinal)
+alert(`The perimeter of the square is: ${square * 4} and the rectangle area is: ${rectangleHight * rectangleWidht}`)
