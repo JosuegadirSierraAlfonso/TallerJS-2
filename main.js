0893baa2fd7c60c7303ee76bc60f022996499527
JosuegadirSierraAlfonso <josuegsierraa18@gmail.com>
2023-03-13 07:28:49 -0500

    feat: :rocket: excersice 9
    N atletas han pasado a finales en salto triple en los juegos olímpicos femenino de 2022. Diseñe un programa que pida por teclado los nombres de cada atleta finalista y a su vez, sus marcas del salto en metros. Informar el nombre de la atleta campeona que se quede con la medalla de oro y si rompió récord, reportar el pago que será de 500 millones. El récord esta en 15,50 metros.

--- main.js
+++ main.js
@@ -1,12 +1,26 @@
-//                  excersice 7
-/* Programa que Ingrese por teclado:
-a. el valor del lado de un cuadrado para mostrar por pantalla el
-perímetro del mismo
-b. la base y la altura de un rectángulo para mostrar el área del
-mismo*/
+//                  excersice 9
+/* N atletas han pasado a finales en salto triple en los juegos
+olímpicos femenino de 2022. Diseñe un programa que pida por
+teclado los nombres de cada atleta finalista y a su vez, sus
+marcas del salto en metros. Informar el nombre de la atleta
+campeona que se quede con la medalla de oro y si rompió
+récord, reportar el pago que será de 500 millones. El récord
+esta en 15,50 metros.*/
 
-let square = parseInt(prompt("Number of one side of the square: "));
-let rectangleHight = parseInt(prompt("Hight of the rectangle"));
-let rectangleWidht = parseInt(prompt("Widht of the rectangle"));
 
-alert(`The perimeter of the square is: ${square * 4} and the rectangle area is: ${rectangleHight * rectangleWidht}`)
+let names = []
+let jump = []
+do {
+  names.push(prompt("Enter a name: "))
+  jump.push(parseFloat(prompt("Enter jump mark: ")))
+} while (confirm("wanna continue? "));
+let bestJump = jump.indexOf(Math.max(...jump))
+
+if (jump[bestJump
+] > 15.5){
+    alert(`${names[bestJump]} you won 500 million for breaking the world record`)
+}else if(jump[bestJump] <= 15.5){
+    alert(`${names[bestJump]} you won the gold medal`)
+}else{
+    alert("ERROR")
+}
