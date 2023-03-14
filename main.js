f4760086e8ca22c7c2f26b3e3b76bfdd3eec2fb4
JosuegadirSierraAlfonso <josuegsierraa18@gmail.com>
2023-03-14 06:35:18 -0500

    feat: :rocket: excersice 10
    Desarrolle un programa cíclico que capture un dato numérico cada vez, y los vaya acumulando. El programa se detiene cuando el usuario digita un cero. El programa debe mostrar: LA SUMATORIA DE LOS VALORES, EL VALOR DEL PROMEDIO, CUÁNTOS VALORES FUERON DIGITADOS, MAYOR VALOR Y MENOR VALOR

--- main.js
+++ main.js
@@ -1,26 +1,34 @@
-//                  excersice 9
-/* N atletas han pasado a finales en salto triple en los juegos
-olímpicos femenino de 2022. Diseñe un programa que pida por
-teclado los nombres de cada atleta finalista y a su vez, sus
-marcas del salto en metros. Informar el nombre de la atleta
-campeona que se quede con la medalla de oro y si rompió
-récord, reportar el pago que será de 500 millones. El récord
-esta en 15,50 metros.*/
+//                  excersice 10
+/* Desarrolle un programa cíclico que capture un dato
+numérico cada vez, y los vaya acumulando. El programa se
+detiene cuando el usuario digita un cero. El programa debe
+mostrar: LA SUMATORIA DE LOS VALORES, EL VALOR DEL
+PROMEDIO, CUÁNTOS VALORES FUERON DIGITADOS, MAYOR
+VALOR Y MENOR VALOR.*/
 
 
-let names = []
-let jump = []
-do {
-  names.push(prompt("Enter a name: "))
-  jump.push(parseFloat(prompt("Enter jump mark: ")))
-} while (confirm("wanna continue? "));
-let bestJump = jump.indexOf(Math.max(...jump))
+let numbers = []
 
-if (jump[bestJump
-] > 15.5){
-    alert(`${names[bestJump]} you won 500 million for breaking the world record`)
-}else if(jump[bestJump] <= 15.5){
-    alert(`${names[bestJump]} you won the gold medal`)
-}else{
-    alert("ERROR")
+while (true) {
+    let enter = (parseInt(prompt("Enter number: ")))
+    numbers.push(enter)
+    if (enter == 0){
+        numbers.pop()
+        if (true){
+            break
+        }
+    }
 }
+
+let sum = numbers.reduce(function(a, b){
+    return a + b; }, 0);
+
+let average = parseInt((sum / (numbers.length)));
+let maxNumber = numbers.indexOf(Math.max(...numbers));
+let minNumber = numbers.indexOf(Math.min(...numbers));
+
+alert(`The sum of all the values is: ${sum}\n
+the average of the values is: ${average}\n
+the number of total added values were: ${numbers.length}\n
+the greater number is: ${numbers[maxNumber]}\n
+and the  lesser number is: ${numbers[minNumber]}`)
